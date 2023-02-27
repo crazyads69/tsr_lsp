46b5bfb4ac1681898c7c7a43326f7ab2e6d04c78
crazyads69 <leminhtri11a1@gmail.com>
2023-02-27 12:36:26 +0700

    Working on Intro Screen UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'dart:async';
+import 'package:tsr_lsp/home_screen.dart';
+import 'package:tsr_lsp/style.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:vector_graphics/vector_graphics.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,105 +17,61 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Tensoract List Port Server',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const SplashScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<SplashScreen> createState() => SplashScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+class SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    new Timer(new Duration(milliseconds: 2000), () {
+      // set your desired delay time here
+      Navigator.of(context).pushReplacement(
+          new MaterialPageRoute(builder: (context) => new HomeScreen()));
     });
   }
 
-  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Container(
+      margin: EdgeInsets.only(
+          top: MediaQuery.of(context).size.height * 20 / 100,
+          left: MediaQuery.of(context).size.width * 10 / 100,
+          right: MediaQuery.of(context).size.width * 10 / 100),
+      width: double.maxFinite,
+      height: double.maxFinite,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SvgPicture(
+            AssetBytesLoader(LogoTSR_precompile),
+            height: MediaQuery.of(context).size.height * 40 / 100,
+            width: MediaQuery.of(context).size.width * 60 / 100,
+          ),
+          Text("TENSORACT", style: boldPCTextStylebl),
+          Padding(
+            padding: EdgeInsets.only(
+                bottom: MediaQuery.of(context).size.height * 10 / 100),
+          ),
+          const SpinKitWave(
+            color: Colors.black,
+            size: 50,
+            duration: const Duration(milliseconds: 2000),
+          )
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
---
+++ lib/style.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+//Color constants
+Color blueColor = const Color(0xFF0081C9);
+Color lightblueColor = const Color(0xFF5BC0F8);
+Color babyblueColor = const Color(0xFF86E5FF);
+
+//Logo & image constants
+String LogoTSR = "assets/image/logo_tsr.svg";
+String LogoTSR_precompile = "image/logo_tsr.svg.vec";
+
+//Typography white constants
+TextStyle boldMobileTextStyle = const TextStyle(
+    fontFamily: "Roboto",
+    fontWeight: FontWeight.bold,
+    fontSize: 16,
+    color: Colors.white);
+TextStyle boldPCTextStyle = const TextStyle(
+    fontFamily: "Roboto",
+    fontWeight: FontWeight.bold,
+    fontSize: 30,
+    color: Colors.white);
+
+TextStyle italicMobileTextStyle = const TextStyle(
+    fontFamily: "Roboto",
+    fontStyle: FontStyle.italic,
+    fontSize: 14,
+    color: Colors.white);
+TextStyle italicPCTextStyle = const TextStyle(
+  fontFamily: "Roboto",
+  fontSize: 20,
+  fontStyle: FontStyle.italic,
+  color: Colors.white,
+);
+
+TextStyle mediumMobileTextStyle = const TextStyle(
+  fontFamily: "Roboto",
+  fontStyle: FontStyle.normal,
+  fontSize: 14,
+  color: Colors.white,
+);
+TextStyle mediumPCTextStyle = const TextStyle(
+  fontFamily: "Roboto",
+  fontSize: 16,
+  fontStyle: FontStyle.normal,
+  color: Colors.white,
+);
+
+//Typography black constants
+TextStyle boldMobileTextStylebl = const TextStyle(
+    fontFamily: "Roboto",
+    fontWeight: FontWeight.bold,
+    fontSize: 16,
+    color: Colors.black);
+TextStyle boldPCTextStylebl = const TextStyle(
+    fontFamily: "Roboto",
+    fontWeight: FontWeight.bold,
+    fontSize: 30,
+    color: Colors.black);
+
+TextStyle italicMobileTextStylebl = const TextStyle(
+    fontFamily: "Roboto",
+    fontStyle: FontStyle.italic,
+    fontSize: 14,
+    color: Colors.black);
+TextStyle italicPCTextStylebl = const TextStyle(
+    fontFamily: "Roboto",
+    fontSize: 20,
+    fontStyle: FontStyle.italic,
+    color: Colors.black);
+
+TextStyle mediumMobileTextStylebl = const TextStyle(
+    fontFamily: "Roboto",
+    fontStyle: FontStyle.normal,
+    fontSize: 14,
+    color: Colors.black);
+TextStyle mediumPCTextStylebl = const TextStyle(
+    fontFamily: "Roboto",
+    fontSize: 16,
+    fontStyle: FontStyle.normal,
+    color: Colors.black);
