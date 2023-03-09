bcf6818d28d1b9cbfa28734c0bc5c1ccb116ef5c
crazyads69 <111816472+crazyads69@users.noreply.github.com>
2023-03-09 10:13:29 +0700

    Update dio_get.dart to new address

--- lib/dio_get.dart
+++ lib/dio_get.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 import 'package:dio/dio.dart';
 
 final dio = Dio();
-String url = "http://35.198.231.19:12000/send_json";
+String url = "http://35.198.197.31:12000/send_json";
 Response response = Response(requestOptions: RequestOptions());
 Future getHttp() async {
   response = await dio.get(url,
